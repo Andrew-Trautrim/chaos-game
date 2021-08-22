fce42d89741ca737e5588126a38ca5437b4d5153
andrew-trautrim <atrautrim7@gmail.com>
2021-08-22 13:14:37 -0400

    Add restrictions and variable jump length

--- chaos-game.py
+++ chaos-game.py
@@ -1,12 +1,19 @@
 from graphics import *
+import sys
 import random
 import time
 
 WIDTH = 1500
 HEIGHT = 900
 
+# Restriction 1: the current vertex cannot be chosen in the next iteration
+# Restriction 2: the current vertex cannot be one place away (counter-clockwise) from the previously chosen vertex
+# Restriction 3: the currently chosen vertex cannot neighbor the previously chosen vertex if the two previously chosen vertices are the same
+RESTRICTION = 3
+JUMP = 1/2
+NUM_POINTS = 4
+
 DELAY = 0
-NUM_POINTS = 3
 
 def main():
     win = GraphWin("Chaos Game", WIDTH, HEIGHT)
@@ -16,13 +23,13 @@ def main():
     text.setTextColor('white')
     text.draw(win)
 
-    points = []
+    vertices = []
     for i in range(NUM_POINTS):
         text.setText("Set end point " + str(i + 1) + "/" + str(NUM_POINTS))
         pt = win.getMouse()
         pt.setFill('white')
         pt.draw(win)
-        points.append(pt)
+        vertices.append(pt)
 
     text.setText("Set starting point")
     iter = win.getMouse()
@@ -30,10 +37,12 @@ def main():
     iter.draw(win)
 
     text.setText("")
+    current = -1
+    previous = -1
     while True:
         try: 
-            pt = points[random.choice(range(NUM_POINTS))]
-            iter = getMidPoint(iter, pt)
+            (current, previous, pt) = getNextVertex(current, previous, vertices)
+            iter = getNextPoint(iter, pt)
             iter.setFill('white')
             iter.draw(win)
             time.sleep(DELAY)
@@ -42,7 +51,22 @@ def main():
 
     win.close()
 
-def getMidPoint(p1, p2):
-    return Point((p1.getX() + p2.getX())/2, (p1.getY() + p2.getY())/2)
+def getNextVertex(current, previous, vertices):
+    next = -1
+    if RESTRICTION == 1:
+        next = random.choice([x for x in range(NUM_POINTS) if x != current])
+    elif RESTRICTION == 2:
+        next = random.choice([x for x in range(NUM_POINTS) if x != (current - 1) % NUM_POINTS])
+    elif RESTRICTION == 3:
+        if current == previous:
+            next = random.choice([x for x in range(NUM_POINTS) if x != (current - 1) % NUM_POINTS and x != (current + 1) % NUM_POINTS])
+        else:
+            next = random.choice(range(NUM_POINTS))
+    else:
+        next = random.choice(range(NUM_POINTS))
+    return (next, current, vertices[next])
+
+def getNextPoint(p1, p2):
+    return Point(p1.getX() + (p2.getX() - p1.getX()) * JUMP, p1.getY() + (p2.getY() - p1.getY()) * JUMP)
 
 main()
